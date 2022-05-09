965487d2978889d834da58a9a25741b708b72953
Lucas <aranchipe98@gmail.com>
2022-05-09 12:32:53 -0300

    v 0.0.1

---
+++ app01/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import perfil from './assets/perfil.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={perfil} alt='perfil' />
+    </div>
+  );
+}
+
+export default App;
